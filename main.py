a3843979eda5f60e6a332957803fd77b9107e33a
Valkenr <valkenr@valkenr.com>
2024-09-05 18:12:47 -0700

    adding movement

--- main.py
+++ main.py
@@ -20,6 +20,7 @@ def main():
                 return
         screen.fill("black")
         player.draw(screen)
+        player.update(dt)
         pygame.display.flip()
 
         # limit frames to 60 FPS
